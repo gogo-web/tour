a96470505d51ae4a26edb8d564594d5354c01288
gogoweb <403091697@qq.com>
2019-06-26 20:12:38 +0800

    init

---
+++ src/page/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="home">
+		<home-header></home-header>
+	</div>
+</template>
+
+<script>
+	import homeHeader from './components/homeHeader'
+	export default {
+		name: 'Home',
+		components:{
+			homeHeader
+		}
+	}
+</script>
+
+<style>
+</style>
