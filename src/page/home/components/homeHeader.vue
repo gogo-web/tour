7b615a50ef8cdc9ba7eac603298b64e4e4318eab
gogoweb <403091697@qq.com>
2019-06-27 21:05:32 +0800

    home-header

--- src/page/home/components/homeHeader.vue
+++ src/page/home/components/homeHeader.vue
@@ -1,5 +1,17 @@
 <template>
-	<div class="test">homeHeader</div>
+	<div class="header">
+		<div class="header-back">
+			<span class="iconfont">&#xe641;</span>
+		</div>
+		<div class="header-search">
+			<span class="iconfont">&#xe688;</span>
+			上海海昌海洋公园
+		</div>
+		<div class="header-city">
+			城市
+			<span class="iconfont">&#xe65d;</span>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -9,6 +21,23 @@
 </script>
 
 <style lang="stylus" scoped>
-	.test
-		color:red
+.header
+  line-height:.88rem
+  text-align:center
+  display:flex
+  .header-back
+    float:left
+    width:.9rem
+  .header-search
+    flex:1
+    height:.6rem
+    line-height:.6rem
+    margin-top:.2rem
+    margin-left:.2rem
+    margin-right:.2rem
+    border-radius:.5rem
+    box-shadow:0 0 .01rem .02rem #ccc
+  .header-city
+    float:right
+    width:1.4rem
 </style>
