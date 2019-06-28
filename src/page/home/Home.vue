4a75d77ee49bac57c3a83446ec5069f5d8405bc4
gogoweb <403091697@qq.com>
2019-06-28 22:03:00 +0800

    home-swiper

--- src/page/home/Home.vue
+++ src/page/home/Home.vue
@@ -1,18 +1,29 @@
 <template>
 	<div class="home">
-		<home-header></home-header>
+		<home-header class="home-header"></home-header>
+		<home-swiper></home-swiper>
 	</div>
 </template>
 
 <script>
 	import homeHeader from './components/homeHeader'
+	import homeSwiper from './components/homeSwiper'
 	export default {
 		name: 'Home',
 		components:{
-			homeHeader
+			homeHeader,
+			homeSwiper,
 		}
 	}
 </script>
 
-<style>
+<style lang="stylus" scoped>
+.home
+  position:relative
+  .home-header
+    position:fixed
+    top:0
+    left:0
+    z-index:100
+    width:100%
 </style>
---
+++ src/page/home/components/homeSwiper.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="wrapper">
+		<swiper :options="swiperOption">
+			<swiper-slide v-for="item of headerImg" :key="item.id">
+				<img class="swiper-img" :src="item.imgUrl">
+			</swiper-slide>
+			<div class="swiper-pagination" slot="pagination"></div>
+		</swiper>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'homeSwiper',
+		data(){
+			return {
+				headerImg:[
+					{
+						id:'001',
+						imgUrl:'https://pic5.40017.cn/01/000/31/71/rBANC1sy7-iAaElXAADxiWL4LPo263_520x218_00.jpg'
+					},
+					{
+						id:'002',
+						imgUrl:'https://pic5.40017.cn/03/000/31/0c/rBANB1zlAcKAJMMtAALRm_wVSlk432_520x218_00.jpg'
+					}
+				],
+				swiperOption:{
+					pagination:'.swiper-pagination',
+					loop:true
+				},
+			}
+		},
+	}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet 
+  background:#fff
+.swiper-img
+  width:100%
+</style>
