836c79d655b29cfc78420d036c03f6a39c23c191
gogoweb <403091697@qq.com>
2019-07-01 08:56:18 +0800

    home-catalog

--- src/page/home/components/homeSwiper.vue
+++ src/page/home/components/homeSwiper.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="wrapper">
 		<swiper :options="swiperOption">
-			<swiper-slide v-for="item of headerImg" :key="item.id">
+			<swiper-slide v-for="item of swiperList" :key="item.id">
 				<img class="swiper-img" :src="item.imgUrl">
 			</swiper-slide>
 			<div class="swiper-pagination" slot="pagination"></div>
@@ -14,7 +14,7 @@
 		name: 'homeSwiper',
 		data(){
 			return {
-				headerImg:[
+				swiperList:[
 					{
 						id:'001',
 						imgUrl:'https://pic5.40017.cn/01/000/31/71/rBANC1sy7-iAaElXAADxiWL4LPo263_520x218_00.jpg'
